170d8d6558ab99f59b64bbbde90c8e4618e56377
Nguyễn Phúc Khang <khang2073@gmail.com>
2024-12-28 20:36:46 +0700

    Adjust training configurations & add weights

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import ultralytics
 ultralytics.checks()
 
 """## Train model"""
-subprocess.run(['yolo','task=classify', 'mode=train', 'model=weight/yolo11m-cls.pt','data=/home/datpham/datpham/ACNE11-Classification/datasets','epochs=200', 'batch=16', 'imgsz=640', 'save=True', 'save_period=10', 'patience=0'])
+subprocess.run(['yolo','task=classify', 'mode=train', 'model=weight/yolo11s-cls.pt','data=/home/datpham/datpham/ACNE11-Classification/datasets','epochs=100', 'batch=16', 'imgsz=800', 'save=True', 'save_period=10', 'patience=0'])
 
 """## Validating model"""
 with open('valid.txt', 'w') as f:
-    result = subprocess.run(['yolo', 'task=classify', 'mode=val', 'model=runs/classify/train/weights/best.pt', 'imgsz=640', 'data=/home/datpham/datpham/ACNE11-Classification/datasets'], capture_output=True, text=True)
+    result = subprocess.run(['yolo', 'task=classify', 'mode=val', 'model=runs/classify/train/weights/best.pt', 'imgsz=800', 'data=/home/datpham/datpham/ACNE11-Classification/datasets'], capture_output=True, text=True)
     f.write(result.stdout)
     f.write(result.stderr)
